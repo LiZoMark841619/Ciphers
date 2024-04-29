692c95c20c807db1e46e393866462cda8642255b
LiZoMark841619 <githubs001@gmail.com>
2024-04-29 21:33:04 +0200

    ok

--- files/ciphers.py
+++ files/ciphers.py
@@ -1,9 +1,9 @@
 class Cipher:
     
-    def __init__(self, msg: str, kind: str):
+    def __init__(self, msg: str, kind: str) -> None:
         self.msg = msg
         self.kind = kind
         self.alpha = ''.join([chr(i) for i in range(97, 97+26)])
 
     def __str__(self) -> str:
-        return f'{self.__class__.__base__.__name__}({self.kind}) object with original message: {self.msg}'
+        return f'Cipher({self.kind}) object with original message: {self.msg}'
